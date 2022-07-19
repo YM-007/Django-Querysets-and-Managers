a1d9e3e4e3d066108425c55c353fb9f152a02f86
YM-007 <yusuf.i.muazu@gmail.com>
2022-07-19 11:51:28 +0100

    second commit

--- I4G023083AV3/links/models.py
+++ I4G023083AV3/links/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from .managers import ActiveLinkManager
 
 
 # Create your models here.
@@ -10,6 +11,8 @@ class Link(models.Model):
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_date = models.DateTimeField()
     active = models.BooleanField(default=True)
+    objects = models.Manager()
+    public = ActiveLinkManager()
 
     def __str__(self):
         return self.description
